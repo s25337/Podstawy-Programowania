db8514dfc0b1e96508325628271d6540cc4bffe2
s25337 <s25337@pjwstk.edu.pl>
2021-10-10 18:18:55 +0200

    first

---
+++ s01-sub.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    if(argc<3) return 0;
+
+    auto const a=std::stoi(argv[1]);
+    auto const b=std::stoi(argv[2]);
+    std::cout<<(a-b)<<"\n";
+    return 0;
+
+}
