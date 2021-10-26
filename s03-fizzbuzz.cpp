25c21d0e66e8ef973c4ca46bec4695dcc270ea25
s25337 <s25337@pjwstk.edu.pl>
2021-10-26 17:11:13 +0200

    loop changes

--- s03-fizzbuzz.cpp
+++ s03-fizzbuzz.cpp
@@ -4,8 +4,12 @@
 int main(int argc, char* argv[])
 {
     auto const a=std::stoi(argv[1]);
-    if (a%3==0) std::cout<<"Fizz";
-    if (a%5==0) std::cout<<"Buzz";
+    
+    for(int i=1; i<=a; i++){
+		std::cout<<i<<" ";
+    if (i%3==0) std::cout<<"Fizz";
+    if (i%5==0) std::cout<<"Buzz";
     std::cout<<"\n";
+}
 
 }
--- s03-guessing-game.cpp
+++ s03-guessing-game.cpp
@@ -8,14 +8,13 @@ std::random_device rd;
 std::uniform_int_distribution<int> guess (1, 100);
 int x=guess(rd);
 int y=0;
-
-while(x!=y){
- std::cout<<"guess:";
+do{
+std::cout<<"guess:";
 std::cin>>y;
 if(y>x) std::cout<<"too big!\n";
-if(y<x) std::cout<<"too small!\n";
+else if(x>y) std::cout<<"too small!\n";
 
-}
+}while(x!=y);
  std::cout<<"just right!\n";
 return 0;
 }
